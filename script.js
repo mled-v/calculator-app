3f8fba5df05eddcd3b250cdc3441a1b01a1afa0f
Matthew <matthewtledford@gmail.com>
2022-07-04 22:20:54 -0500

    Added more functions to calculator app

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = document.querySelector("#display")
+
 const add = function(x,y) {
     let ans = x + y
     return ans
@@ -21,9 +23,26 @@ const divide = function(x,y) {
     return ans
 }
 
+const operate = function (num1, num2, operater){
+    switch (operater){
+        case '+':
+            return add(num1,num2);
+        case '-':
+            return subtract(num1,num2)
+        case '*':
+            return multiply(num1,num2)
+        case '/':
+            return divide(num1,num2)
+    }
+
+
+
+}
+
 
 
 console.log(add(2,2))
 console.log(subtract(5,2))
 console.log(multiply(5,5))
 console.log(divide(25,5))
+console.log(operate(5,6,'-'))
